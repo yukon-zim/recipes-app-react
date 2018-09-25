2f06845600b5d2ffcf786bce309208335b2e7d97
Jen Zimmerman <jenniferlzim@gmail.com>
2018-09-25 16:07:46 -0500

    setup unit tests for import-csv component

---
+++ src/import-recipe/import-csv.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow, render } from 'enzyme';
+import recipeFixtures from '../testing/recipe-fixtures.js';
+import ImportCsv from './import-csv';
+
+describe('component tests', () => {
+    describe('render tests', () => {
+        it('initial render prior to file selection', () => {
+
+        });
+        it('rendering on selecting a file and cancelling import', () => {
+
+        });
+        it('rendering on clicking import button', () => {
+
+        })
+    })
+});
