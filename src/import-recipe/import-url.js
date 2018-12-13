0a48b171f22954e1b343f5494f71263089aa0705
Jen Zimmerman <jenniferlzim@gmail.com>
2018-12-13 17:12:30 -0600

    wired up import from URL mutation, error handling

--- src/import-recipe/import-url.js
+++ src/import-recipe/import-url.js
@@ -1,60 +1,53 @@
 import React, {Component} from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { ALL_RECIPES_QUERY } from '../recipe-list/recipe-list'
+
+const URL_IMPORT_MUTATION = gql`
+    mutation URL_IMPORT_MUTATION($url: String!) {
+        importRecipeFromUrl(url: $url) {
+            name
+        }
+    }
+`;
 
 export default class ImportUrl extends Component {
     constructor(props) {
         super(props);
         this.state = {
             urlInputValid: false,
-            urlImportError: ''
+            urlImportMessage: ''
         }
     }
-    async importRecipeByUrl(url) {
+    async importRecipeByUrl(importUrlMutation) {
         try {
-            this.importUrlRequestPromise = this.importUrlRecipeRequest(url);
-            const importResponse = await this.importUrlRequestPromise;
-            this.getRecipesPromise = this.props.getRecipes();
-            const recipes = await this.getRecipesPromise;
+            const res = await importUrlMutation({
+                variables: {url: this.urlToImportInput.value}
+            });
             this.cancelUrlImport();
-            this.props.setRecipes(recipes);
             this.setState({
-                urlImportError: importResponse.message
+                urlImportMessage: 'Successfully imported recipe from URL!'
             });
+            return res;
         } catch (err) {
-            console.log(err);
-            this.setState({
-                urlImportError: err.message
-            });
+            console.error(err);
         }
-    }
-
-    async importUrlRecipeRequest(url) {
-        const response = await fetch(`http://localhost:1337/recipes/import-url`, {
-            method: 'POST',
-            body: JSON.stringify({url})
-        });
-        const jsonData = await response.json();
-        if (response.ok) {
-            return jsonData;
-        }
-        throw new Error(`${jsonData.name} ${jsonData.statusCode} - check api logs`)
     }
 
     cancelUrlImport() {
         this.urlToImportInput.value = null;
         this.setState({
             urlInputValid: false,
-            urlImportError: ''
+            urlImportMessage: ''
         })
     }
 
     onAddUrl(urlInput) {
-        // if (!this.urlToImportInput === undefined && this.urlToImportInput.validity.valid) {
         if (urlInput.value) {
             this.setState({
                 urlInputValid: true
             })
         }
-        // }
     }
 
     handleSubmit(event) {
@@ -69,29 +62,37 @@ export default class ImportUrl extends Component {
         return(
             <div>
                 <div>
-                    <form onSubmit={this.handleSubmit}>
-                        <div>
-                            <label htmlFor="url-file-upload">Import recipes by URL:</label>
-                        </div>
-                        <div>
-                            <input id="url-file-upload" type="url" required
-                                   ref={textInput => this.urlToImportInput = textInput}
-                                   onChange={(event) => this.onAddUrl(event.target)}/>
-                        </div>
-                        <div>
-                            <button className="btn btn-secondary btn-sm btn-import-url"
-                                    disabled={!this.state.urlInputValid}
-                                    onClick={() => this.importRecipeByUrl(this.urlToImportInput.value)}>Import web recipe
-                            </button>
-
-                        </div>
-                        <div>
-                                <span className="error-message url-import-error">
-                                    {this.state.urlImportError}
+                    <Mutation mutation={URL_IMPORT_MUTATION}
+                              refetchQueries={[{query: ALL_RECIPES_QUERY, variables: {searchTerm: ''}}]}>
+                        {(importUrl, {error, loading}) => (
+                            <form onSubmit={this.handleSubmit}>
+                                <div>
+                                    <label htmlFor="url-file-upload">Import recipes by URL:</label>
+                                </div>
+                                <span className="badge-success">
+                                    {this.state.urlImportMessage}
                                 </span>
-                        </div>
-                    </form>
+                                <div>
+                                    <input id="url-file-upload" type="url" required
+                                           ref={textInput => this.urlToImportInput = textInput}
+                                           onChange={(event) => this.onAddUrl(event.target)}/>
+                                </div>
+                                <div>
+                                    <button className="btn btn-secondary btn-sm btn-import-url"
+                                            disabled={!this.state.urlInputValid}
+                                            onClick={() => this.importRecipeByUrl(importUrl)}>Import web recipe
+                                    </button>
+                                </div>
+                                <div>
+                                    {error && (
+                                        <p className="error-message">An error ocurred while importing the recipe. Check the console.</p>
+                                    )}
+                                </div>
+                            </form>
+                        )}
+                    </Mutation>
                 </div>
             </div>
-        )}
+        )
+    }
 }
